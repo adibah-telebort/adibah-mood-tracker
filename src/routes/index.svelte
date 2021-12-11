0c63468032ed92abda3cfc748dff774697fa277b
adibah-telebort <adibah.telebort@gmail.com>
2021-12-11 18:45:50 +0000

    part9

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,6 +9,14 @@
 
    	    if (error) alert(error.message); // alert if error
     }
+
+    // Select entries
+    async function getEntries() {
+        const { data, error } = await supabase.from('moodEntries').select();
+        if (error) alert(error.message);
+
+        return data;
+    }
 </script>
 
 <Greeting />
@@ -27,7 +35,20 @@
 
     <div class="list-group mb-3">
    	 <!-- Individual Entries -->
-    	<Entry />
+    {#await getEntries()}
+        <p>Fetching data...</p>
+    {:then data}
+        {#each data as entry}
+            <Entry
+                date={entry.day + '-' + entry.month + '-' + entry.year}
+                mood={entry.mood}
+                comment={entry.comment}
+            />
+        {/each}
+    {:catch error}
+        <p>Something went wrong while fetching the data:</p>
+        <pre>{error}</pre>
+    {/await}
    	 
     </div>
 </section>
